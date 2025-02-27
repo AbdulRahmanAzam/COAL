0ceeb3e0418daa2ad432c21e1e5cf531a88263b6
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2025-02-27 14:11:07 +0500

    Update Task 1.asm

--- Lab 2/Task 1.asm
+++ Lab 2/Task 1.asm
@@ -1,3 +1,26 @@
+1) 
+INCLUDE Irvine32.inc
+
+.code
+main PROC
+	mov eax, 47 + 39 + 60 + 85 + 64 + 054o - 0Ah
+
+	mov ebx, 30 - 9 + 186 - 150
+
+	mov ecx, 101110 + 50Ah + 6710d + 1010001 + 0Fh
+
+	mov edx, 10001101 - 0D83h + 385+10 + 1111101 - 0Eh+0Fh
+
+	mov esi, 101b - 9 + 1A4h - 560o
+
+	call DumpRegs
+	exit
+main ENDP
+END main
+
+
+OR 
+
 1)
 include irvine32.inc
 .data
@@ -17,6 +40,3 @@ main proc
 	exit
 main endp
 end main
-
-
-2)
