767b246b6b0d5b319c536493ec90c8865ec5b14c
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2025-03-10 09:29:20 +0500

    Update 1.asm

--- Lab 5/1.asm
+++ Lab 5/1.asm
@@ -1,1 +1,18 @@
+.data
+    val1 SDWORD 8000      
 
+.code
+main PROC
+    mov eax, val1        
+    add eax, 1            
+
+    jc CarryFlagSet       
+    js SignFlagSet        
+
+    exit
+CarryFlagSet
+    exit
+SignFlagSet:
+    exit
+main ENDP
+END main
